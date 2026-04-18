87f00f874dff9df57f325e3618ad6c86efb58210
JFKDD <gfddtuu56@gmail.com>
2026-04-18 23:00:12 +0300

    2

--- config.php
+++ config.php
@@ -1,15 +1,18 @@
 <?php
-$host = "localhost"; // غالباً يبقى هكذا أو تزودك الاستضافة بعنوان معين
-$user = "اسم_المستخدم_الجديد"; 
-$pass = "كلمة_السر_الجديدة";
-$db   = "اسم_قاعدة_البيانات_الجديدة";
+// بيانات الاتصال من Clever Cloud
+<?php
+$host = "bdsdtmnvvctt2bgn66ua-mysql.services.clever-cloud.com";
+$user = "ugafqtnyw3dl5ct6";
+$pass = "lDtxgtyMtyGSbnX7d61R";
+$db   = "bdsdtmnvvctt2bgn66ua";
+$port = 3306; // أضف المنفذ هنا
 
-$conn = new mysqli($host, $user, $pass, $db);
+$conn = new mysqli($host, $user, $pass, $db, $port);
 
 if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
+    die("فشل الاتصال: " . $conn->connect_error);
 }
-
+// ... بقية الكود
 // دالة لجلب الـ IP الحقيقي
 function get_client_ip() {
     if (!empty($_SERVER['HTTP_CLIENT_IP'])) {
